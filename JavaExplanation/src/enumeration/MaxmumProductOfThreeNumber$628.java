7a9a2594ab51cce6d5d52575aac14dbaf7b3552d
MorningSun-GitBoy <AndyWolfcf2013@163.com>
2021-02-11 10:00:49 +0800

    628. maxmum product of three number enumeration method

--- JavaExplanation/src/enumeration/MaxmumProductOfThreeNumber$628.java
+++ JavaExplanation/src/enumeration/MaxmumProductOfThreeNumber$628.java
@@ -8,9 +8,11 @@ import java.util.Scanner;
 
 public class MaxmumProductOfThreeNumber$628 {
     public static void main(String[] args) throws FileNotFoundException {
+        String inputFile = "CaseFiles\\Enumeration\\MaxmumProductOfThreeNumber_628_Input";
+        String outputFile = "CaseFiles\\Enumeration\\MaxmumProductOfThreeNumber_628_Output";
         Solution s = new Solution();
-        Scanner sc = new Scanner(new FileInputStream(""));
-        Scanner so = new Scanner(new FileInputStream(""));
+        Scanner sc = new Scanner(new FileInputStream(inputFile));
+        Scanner so = new Scanner(new FileInputStream(outputFile));
         while(sc.hasNext()){
             String inputStr = sc.nextLine();
             int outputInt = Integer.valueOf(so.nextLine());
@@ -20,6 +22,16 @@ public class MaxmumProductOfThreeNumber$628 {
         }
     }
     private static class Solution {
+        /**
+         *  choose three element from one array , make the product of them biggest
+         * @param  nums  -1000<=nums[i]<=1000  3<=nums.length<=1000
+         * @return the maxmum product of three elements
+         * @TimeComplexity O(n)
+         * @SpaceComplexity O(1)
+         * @Since 2021-02-11
+         * @TimeOver    82.94%
+         * @SpaceOver   91.48%
+         */
         public int maximumProduct(int[] nums) {
             int     min_1 = Integer.MAX_VALUE,
                     min_2 = Integer.MAX_VALUE;
@@ -30,11 +42,18 @@ public class MaxmumProductOfThreeNumber$628 {
                 if(min_1 > nums[i]){
                     min_2 = min_1;
                     min_1 = nums[i];
+                }else if(min_2 > nums[i]){
+                    min_2 = nums[i];
                 }
                 if(max_1 < nums[i]){
                     max_3 = max_2;
                     max_2 = max_1;
                     max_1 = nums[i];
+                }else if(max_2 < nums[i]){
+                    max_3 = max_2;
+                    max_2 = nums[i];
+                }else if(max_3 < nums[i]){
+                    max_3 = nums[i];
                 }
             }
             return Math.max(min_1*min_2*max_1,max_1*max_2*max_3);
